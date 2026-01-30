02ff94345c4d1f6aafc532e827c66fb304fa6afc
Alamak <mizunarei60@gmail.com>
2026-01-30 16:11:01 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    titleWeb: "Confess Crush",
+    introTitle: 'Hai maniesss!',
+    introDesc: `jadi gini aku ga tau nih mau ngapain buat nembah jadi ku bikin ini aja deh yaa yang simpel ajahh hehe`,
+    btnIntro: 'Pencet Dong',
+    title: 'mau jadi pacar aku ga maniess 🥰',
+    desc: 'ku tantang bilang enggak kalo bisa ',
+    btnYes: 'Okii mauu <33',
+    btnNo: 'Ga mau ahh',
+    question: 'Apa alasanya mau jadi pacarku <3',
+    btnReply: 'pencet dong <3',
+    reply: 'Karena aku sayang sama kamu <33333333',
+    mess: 'yeyyy sekarang kita jadian 🥰😘😘',
+    messDesc: 'walaupun aku ga peka moga betah yahh.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://bersamarei.github.io/kado/' 
+}
+a
